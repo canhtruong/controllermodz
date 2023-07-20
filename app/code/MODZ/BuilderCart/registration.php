6bdbef583adceea7dc4b4888b27f9b5c8cd54621
Truong Canh <magedev89@gmail.com>
2023-07-21 03:53:50 +0700

    Update app folderr

--- app/code/MODZ/BuilderCart/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'MODZ_BuilderCart',
-    __DIR__
-);
--- app/code/MODZ/CheckoutAddressesLabels/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'MODZ_CheckoutAddressesLabels',
-    __DIR__
-);
